57d49f5df7095a762ab9d949640489667347bcb8
Chaimaaorg <chaimaaourgani84@gmail.com>
2025-06-02 14:08:18 +0100

    Add python version for deployment

--- backend/main.py
+++ backend/main.py
@@ -50,6 +50,4 @@ async def transcribe_audio(file: UploadFile = File(...)):
 
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    
